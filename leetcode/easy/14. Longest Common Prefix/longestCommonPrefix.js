04633035157f6b88502aa5551079c87c0aeb85f0
yoosaemsol <yoosaemsol@gmail.com>
2023-09-02 23:30:25 +0900

    Create 14. Longest Common Prefix (Accepted/c)

---
+++ leetcode/easy/14. Longest Common Prefix/longestCommonPrefix.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) {
+    return '';
+  }
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix[j] === strs[i][j]
+    ) {
+      j++;
+    }
+
+    prefix = prefix.slice(0, j);
+
+    if (prefix === '') {
+      break;
+    }
+  }
+
+  return prefix;
+};
