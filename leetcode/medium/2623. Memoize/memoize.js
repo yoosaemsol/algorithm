63569ff98dafe1f5222cc67b40269623ffb74f93
yoosaemsol <yoosaemsol@gmail.com>
2023-09-05 23:46:03 +0900

    Update 2623. Memoize (Accepted)

--- leetcode/medium/2623. Memoize/memoize.js
+++ leetcode/medium/2623. Memoize/memoize.js
@@ -7,7 +7,7 @@ function memoize(fn) {
   return function (...args) {
     const key = JSON.stringify(args);
 
-    if (cache?.[key] !== undefined) {
+    if (key in cache) {
       return cache[key];
     }
     const result = fn(...args);
