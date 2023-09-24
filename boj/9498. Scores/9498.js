2b9197c34e1b7b72bad4b7188b7c77454a3aee8f
yoosaemsol <yoosaemsol@gmail.com>
2023-09-25 00:02:59 +0900

    Update 9498. Scores (Accepted)

--- boj/9498. Scores/9498.js
+++ boj/9498. Scores/9498.js
@@ -1,20 +1,14 @@
-let fs = requires('fs');
+let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 const score = Number(input[0]);
 
 function check(a) {
-  if (90 <= a) {
-    console.log('A');
-  } else if (80 <= a) {
-    console.log('B');
-  } else if (70 <= a) {
-    console.log('C');
-  } else if (60 <= a) {
-    console.log('D');
-  } else {
-    console.log('F');
-  }
+  if (90 <= a) console.log('A');
+  else if (80 <= a) console.log('B');
+  else if (70 <= a) console.log('C');
+  else if (60 <= a) console.log('D');
+  else console.log('F');
 }
 
 check(score);
