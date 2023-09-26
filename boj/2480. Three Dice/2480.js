a0c80e59f84f0be9bcdae95f0f432155195063b5
yoosaemsol <yoosaemsol@gmail.com>
2023-09-26 13:14:46 +0900

    Update 2480. Three Dice (Accepted)

--- boj/2480. Three Dice/2480.js
+++ boj/2480. Three Dice/2480.js
@@ -2,19 +2,15 @@ let fs = require('fs');
 let inputLines = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let [a, b, c] = inputLines[0].split(' ').map(Number);
-let prize;
 
 if (a === b && a === c) {
-  prize = 10000 + a * 1000;
+  console.log(10000 + a * 1000);
 } else if (a !== b && a !== c && b !== c) {
-  const highest = [a, b, c].sort((a, b) => b - a)[0];
-  prize = highest * 100;
+  console.log(Math.max(a, b, c) * 100);
 } else {
   if (a === b) {
-    prize = 1000 + a * 100;
+    console.log(1000 + a * 100);
   } else {
-    prize = 1000 + c * 100;
+    console.log(1000 + c * 100);
   }
 }
-
-console.log(prize);
