dca7543b14d08b2d656471fc9ee7de798f76333a
yoosaemsol <yoosaemsol@gmail.com>
2023-09-27 13:13:21 +0900

    Update 10818. Minimum, Maximum (Accepted)

--- boj/10818. Minimum, Maximum/10818.js
+++ boj/10818. Minimum, Maximum/10818.js
@@ -1,9 +1,15 @@
 let fs = require('fs');
 let inputLines = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let length = Number(inputLines[0]);
-let numbers = inputLines[1].split(' ').map(Number);
+let n = Number(inputLines[0]);
+let arr = inputLines[1].split(' ').map(Number);
 
-numbers = numbers.sort((a, b) => a - b);
+let minValue = 1000001;
+let maxValue = -1000001;
 
-console.log(numbers[0], numbers[length - 1]);
+for (let i = 0; i < n; i++) {
+  if (minValue > arr[i]) minValue = arr[i];
+  if (maxValue < arr[i]) maxValue = arr[i];
+}
+
+console.log(minValue, maxValue);
