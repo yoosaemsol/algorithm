82183fbb3afd6f00e35e35c26e25171476874ffe
yoosaemsol <yoosaemsol@gmail.com>
2023-10-02 15:37:36 +0900

    Update 11720. Sum of Digits (Accepted)

--- boj/11720. Sum of Digits/11720.js
+++ boj/11720. Sum of Digits/11720.js
@@ -1,10 +1,10 @@
 let fs = require('fs');
 let inputLines = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let n = Number(inputLines[0]);
 let result = 0;
-for (let i = 0; i < n; i++) {
-  result += Number(inputLines[1][i]);
+
+for (let x of inputLines[1]) {
+  result += Number(x);
 }
 
 console.log(result);
