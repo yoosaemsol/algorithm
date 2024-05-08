5492f14cbe746d79302cacbfab8c37c6ba3addb8
yoosaemsol <yoosaemsol@gmail.com>
2024-05-08 13:58:03 -0400

    Update: 1931. Conference Room Allocation (Accepted/s)

---
+++ boj/1931. Conference Room Allocation/1931-v2.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  arr.push(input[i].split(" ").map(Number));
+}
+
+arr.sort((a, b) => {
+  if (a[1] != b[1]) return a[1] - b[1];
+  else return a[0] - b[0];
+});
+
+let cnt = 1;
+let cur = 0;
+
+for (let i = 1; i < n; i++) {
+  if (arr[cur][1] <= arr[i][0]) {
+    cur = i;
+    cnt += 1;
+  }
+}
+
+console.log(cnt);
